962e17f8b0f58486030a47f237290dab3e0f64e5
andres alcocer <andresalcocer7@yahoo.com>
2019-01-15 13:45:21 -0500

    added socket event functions in Factory.js

---
+++ src/Factories.js
@@ -0,0 +1,50 @@
+/** Library that creates uniques ids */
+const uuidv4 = require('uuid/v4');
+
+/**
+ * Creates a user
+ * @prop id {string}
+ * @prop name { string }
+ * @prop {object}
+ * */
+const createUser = ({name = ""} = {}) => (
+  {
+    id: uuidv4(),
+    name
+  }
+);
+
+/**
+ * Creates a messages object
+ * */
+const createMessage = ({message = "", sender = ""} = {}) => (
+  {
+    id: uuidv4(),
+    time: getTime(new Date(Date.now())),
+    message,
+    sender
+  }
+);
+
+/**
+ * Create a Chat object
+ * */
+const createChat = ({messages = [], name = "Community", users = []} = {}) => (
+  {
+    id: uuidv4(),
+    name,
+    messages,
+    users,
+    typingUsers: []
+  }
+);
+
+/**
+ *@param date { Date }
+ * @return {string} a string converted to a 24hr time
+ * */
+const getTime = (date) => {
+  return `${date.getHours()}:${("0" + date.getMinutes()).slice(-2)}`;
+};
+
+
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="login">
+
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
